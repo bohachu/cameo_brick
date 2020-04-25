076b8d94bb58b21b11ada4148c579de3bcbf67ce
bohachu <cbh@cameo.tw>
2020-04-25 13:52:25 +0000

    ok

---
+++ 狗雷伯/git_push.sh
@@ -0,0 +1,7 @@
+git remote set-url origin https://cbh%40cameo.tw:Laserr!%40%23456@github.com/bohachu/cameo_brick.git
+git pull
+git add .
+git config --global user.email "cbh@cameo.tw"
+git config --global user.name "bohachu"
+git commit -m ok
+git push
