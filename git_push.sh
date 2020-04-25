3a708aeb62c5caf0938b14d47e08a3de8bdc1d15
bohachu <cbh@cameo.tw>
2020-04-25 14:33:20 +0000

    ok

--- git_push.sh
+++ git_push.sh
@@ -1,8 +1,5 @@
 cd /content/cameo_brick
 
-#update ipynb from google drive to colab content
-wget --no-check-certificate 'https://docs.google.com/uc?export=download&id=1d_ourbLJnhBrfPieLlQNc696Q5Tx9zBe' -O /content/cameo_brick/狗雷伯/狗雷伯積木塊入門.ipynb
-
 #github
 git remote set-url origin https://cbh%40cameo.tw:Laserr!%40%23456@github.com/bohachu/cameo_brick.git
 git pull
